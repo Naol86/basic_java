a087274fdf94640144374d9c5cd2d62b586837c1
naol86 <lijnaolkasinet@gmail.com>
2023-07-03 13:00:33 +0300

    bubble sort

---
+++ bubble.java
@@ -0,0 +1,21 @@
+package basic_java;
+
+public class bubble {
+    public static void main(String[] args) {
+        int num[] = { 3, 4, 1, 5, 6, 0, 9, 7 ,8,200,11,15,10};
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length - i - 1; j++) {
+                if (num[j] > num[j + 1]) {
+                    // swap
+                    int temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                }
+            }
+            for (int k = 0; k < num.length; k++) {
+                System.out.print(num[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
